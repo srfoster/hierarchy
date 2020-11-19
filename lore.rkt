2500858b192f1573f1eb6de781fed56b7fd97c42
srfoster <lindseyhandley@gmail.com>
2020-11-19 15:07:37 -0800

    updates

--- lore.rkt
+++ lore.rkt
@@ -1,7 +1,13 @@
 #lang at-exp racket
 
-(require hierarchy
-	 codespells/lore)
+(require codespells/lore
+         (only-in codespells append-rune-langs
+                             require-mod))
+
+(require-mod hierarchy)
+(require-mod fire-particles)
+(require-mod ice-particles)
+(require-mod rocks)
 
 (define-rune-collection-lore 
   #:name "Translocation and Unification"
@@ -16,6 +22,33 @@
       #:name "Unite"
       #:rune (parentify-rune)
       #:description @md{This rune creates a parent-child relationship between whatever is spawned by subsequent runes in the spell expression.
- The first becomes the parent of the rest.}
-      ))
-  #:preview-image (parentify-rune))
+      The first becomes the parent of the rest.
+      @(rune-code-example
+         (append-rune-langs
+           #:name #f 
+           (hierarchy:my-mod-lang)
+           (ice-particles:my-mod-lang)
+           (fire-particles:my-mod-lang #:with-paren-runes? #t))
+         (parentify 
+           ice-aura
+           fire-beam
+           )
+         hierarchy-demo-1.mp4)
+      
+      You can unite more than 2 runes! Try 3 or more.
+
+      @(rune-code-example
+         (append-rune-langs
+           #:name #f 
+           (rocks:my-mod-lang)
+           (ice-particles:my-mod-lang)
+           (hierarchy:my-mod-lang)
+           (fire-particles:my-mod-lang #:with-paren-runes? #t))
+         (parentify 
+           forest-rock
+           flames
+           ice-ball
+           )
+         hierarchy-demo-2.mp4)}
+  ))
+  #:preview-image preview.png)
